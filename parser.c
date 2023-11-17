3b095baa1210d605b4e4c7021c3d0cad1ae09ad1
peterg127 <peter.gvozdjak1@gmail.com>
2023-11-17 23:32:48 +0100

    Added rules for var and while in parser.

--- parser.c
+++ parser.c
@@ -45,10 +45,14 @@ bool priradenie_prave(FILE* file){
     struct Token current_token2 = getNextToken(file);
     if (current_token.type == 1 && current_token2.type == 9 && strcmp(current_token2.attribute, "(") == 0){
         printf("funkcia priradenie\n");
+        return true;
         //return parametre_volania(file)
     }
     else if (current_token.type == 1 || current_token.type == 2 ||  current_token.type == 3 || current_token.type == 7){
         printf("expression will be reduced\n");
+        printf("%s\n", current_token.attribute);
+        unget_token(current_token2, file);
+        return true;
         //precencna analzya(),druhy token treba vymysliet aby sa znova odnacital
     }
     else{
@@ -79,25 +83,67 @@ bool letnutie(FILE* file){
         return false;
     }
 }
-bool sekvencia(FILE* file){
-    printf("%s\n", current_token.attribute);
-    printf("%s\n", current_token.attribute);
-    //printf("Token 2 type: %d\n", current_token.type);
-    if (strcmp(current_token.attribute, "let") == 0 && current_token.type == 4){
-        return letnutie(file);
+
+bool varnutie(FILE* file){
+    current_token = getNextToken(file);
+    if (current_token.type == 1){
+        return typ_s_dvojbodkou(file) && priradenie(file);
     }
-    else if (current_token.attribute == "var" && current_token.type == 4){
-        //varnutie();
+    else{
+        return false;
     }
-    else if (current_token.attribute == "while" && current_token.type == 4){
-        //whilnutie();
+}
+
+bool whilnutie(FILE* file){
+    bool res;
+    current_token = getNextToken(file);
+    if (current_token.type == 1 || current_token.type == 2 ||  current_token.type == 3 || current_token.type == 7){
+        printf("expression will be reduced\n");
+        //dame na precedencnu analyzu nech zjednoduchsi vyraz alebo podmienku
+        current_token = getNextToken(file);
+        //current_token2 = getNextToken(file);
+        if (current_token.type == 9 && strcmp(current_token.attribute, "{") == 0){
+            current_token = getNextToken(file);
+            res = sekvencia(file);
+        }
+        current_token = getNextToken(file);
+        return res && current_token.type == 9 && strcmp(current_token.attribute, "}") == 0;
     }
     return false;
+
+    
+}
+bool sekvencia(FILE* file){
+    current_token = getNextToken(file);
+    printf("%s\n", current_token.attribute);
+    printf("type: %d\n", current_token.type);
+    if (strcmp(current_token.attribute, "let") == 0 && current_token.type == 4){
+        if (letnutie(file) == false){
+            return false;
+        }
+        return sekvencia(file);
+    }
+    else if (strcmp(current_token.attribute, "var") == 0 && current_token.type == 4){
+        if (varnutie(file) == false){
+            return false;
+        }
+        sekvencia(file);
+    }
+    // else if (strcmp(current_token.attribute, "while") == 0 && current_token.type == 4){
+    //     if (whilnutie(file) == false){
+    //         printf("somt");
+    //         return false;
+    //     }
+    //     sekvencia(file);
+    // }
+    // else{
+    //     return false;           //not sure
+    // }
     //TODO dalsie mozne neterminaly zo sekvencie
 }
 
 void parser(FILE* file){
-    current_token = getNextToken(file);
+    //current_token = getNextToken(file);
     if (sekvencia(file) == true){
         printf("ok");
     }
