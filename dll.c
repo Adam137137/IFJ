60e7311922e5c3d0fb0c398ef0d16f62ce36dc4a
TomasLustik <tomas.lustik@gmail.com>
2023-11-23 16:29:02 +0100

    Expanded DLL funcionality

--- dll.c
+++ dll.c
@@ -17,6 +17,23 @@ void DLL_Dispose( DLList *list ) {
 	list->lastElement = NULL;
 }
 
+void DLL_InsertFirst( DLList *list, int data ) {
+	DLLElementPtr cache = list->firstElement;
+	list->firstElement = malloc(sizeof(struct DLLElement));
+	if(list->firstElement == NULL){
+		handle_error(99);
+	}
+	list->firstElement->data = data;
+	list->firstElement->nextElement = cache;
+	list->firstElement->previousElement = NULL;
+	if(cache != NULL){	//Set ex-first element's pointer to new element
+		cache->previousElement = list->firstElement;
+	}
+	else{	//If inserted element is the sole element in list, set it to be the last element aswell
+		list->lastElement = list->firstElement;
+	}
+}
+
 void DLL_InsertLast( DLList *list, char data ) {
 	DLLElementPtr cache = malloc(sizeof(struct DLLElement));
 	if(cache == NULL){
@@ -35,6 +52,10 @@ void DLL_InsertLast( DLList *list, char data ) {
 		list->lastElement = cache;
 		list->lastElement->nextElement = NULL;
 	}
+}
+
+void DLL_First( DLList *list ) {
+	list->activeElement = list->firstElement;
 }
 
 void DLL_Last( DLList *list ) {
@@ -62,6 +83,47 @@ void DLL_InsertAfter( DLList *list, char data ) {
 	}
 }
 
+void DLL_InsertBefore( DLList *list, int data ) {
+	if(list->activeElement == NULL){	//Check if the list is active
+		return;
+	}
+	DLLElementPtr cache = malloc(sizeof(struct DLLElement));
+	if(cache == NULL){
+		handle_error(99);
+	}
+	cache->data = data;
+	cache->previousElement = list->activeElement->previousElement;
+	cache->nextElement = list->activeElement;
+	list->activeElement->previousElement = cache;
+	//If active element is the first element in list, set inserted element as first element
+	if(list->activeElement == list->firstElement){
+		list->firstElement = cache;
+	}
+	else{
+		cache->previousElement->nextElement = cache;
+	}
+}
+
+char DLL_DeleteFirst( DLList *list ) {
+	if(list->firstElement == NULL){
+		return 0;
+	}
+	if(list->activeElement == list->firstElement){
+		list->activeElement = NULL;
+	}
+	DLLElementPtr cache = list->firstElement;
+	list->firstElement = list->firstElement->nextElement;
+
+	if(list->firstElement != NULL){
+		list->firstElement->previousElement = NULL;		
+	}
+	else{	//If first element was the only element in list, set last element to NULL aswell
+		list->lastElement = NULL;
+	}
+	return cache->data;
+	free(cache);
+}
+
 char DLL_DeleteLast( DLList *list ) {
 	if(list->lastElement == NULL){
 		return 0;
@@ -80,6 +142,48 @@ char DLL_DeleteLast( DLList *list ) {
 	}
 	return cache->data;
 	free(cache);
+}
+
+char DLL_DeleteAfter( DLList *list ) {
+	//Check if the list is active or active element is the last element in list
+	if(list->activeElement == NULL || list->activeElement == list->lastElement){
+		return;
+	}
+	DLLElementPtr cache = list->activeElement->nextElement;
+	list->activeElement->nextElement = cache->nextElement;
+	if(cache == list->lastElement){	//If element after active is the last element, set active to the last
+		list->lastElement = list->activeElement;
+	}
+	else
+	{
+		cache->nextElement->previousElement = list->activeElement;
+	}
+	return cache->data;
+	free(cache);
+}
+
+char DLL_DeleteBefore( DLList *list ) {
+	//Check if the list is active or active element is the first element in list
+	if(list->activeElement == NULL || list->activeElement == list->firstElement){
+		return;
+	}
+	DLLElementPtr cache = list->activeElement->previousElement;
+	list->activeElement->previousElement = cache->previousElement;
+	if(cache == list->firstElement){	//If element before active is the first element, set active to the first
+		list->firstElement = list->activeElement;
+	}
+	else{
+		cache->previousElement->nextElement = list->activeElement;
+	}
+	return cache->data;
+	free(cache);
+}
+
+char DLL_GetFirst( DLList *list, int *dataPtr ) {
+	if(list->firstElement == NULL){
+		handle_error(99);
+	}
+	return list->firstElement->data;
 }
 
 char DLL_GetLast( DLList *list){
