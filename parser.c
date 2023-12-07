bada54d9aaf38a94fbbe872cdf2d90935709ac73
mariop1919 <marioperecz@gmail.com>
2023-12-07 01:29:04 +0100

    fixed strlen func

--- parser.c
+++ parser.c
@@ -528,13 +528,13 @@ bool priradenie_prave(char *name_of_node){
                 variable_name = unique_name(name_of_node, 0);
                 sprintf(buffer1.data, "%sDEFVAR GF@TEMP%d\n", buffer1.data, built_in_counter + func_counter);
                 sprintf(buffer1.data, "%sPOPS GF@TEMP%d\n", buffer1.data, built_in_counter + func_counter);
-                sprintf(buffer1.data, "%sSTRLEN %s GF@TEMP%d\n", buffer1.data, variable_name, built_in_counter + func_counter);    
+                sprintf(buffer1.data, "%sSTRLEN GF@%s GF@TEMP%d\n", buffer1.data, variable_name, built_in_counter + func_counter);    
             }
             else if(frame_counter-anti_zanorenie > 0){                             // assignment after checks
                 variable_name = unique_name(name_of_node, frame_counter-anti_zanorenie);
                 printf(buffer1.data, "%sDEFVAR LF@TEMP%d\n", buffer1.data, built_in_counter + func_counter);
                 sprintf(buffer1.data, "%sPOPS LF@TEMP%d\n", buffer1.data, built_in_counter + func_counter);
-                sprintf(buffer1.data, "%sSTRLEN %s LF@TEMP%d\n", buffer1.data, variable_name, built_in_counter + func_counter);
+                sprintf(buffer1.data, "%sSTRLEN LF@%s LF@TEMP%d\n", buffer1.data, variable_name, built_in_counter + func_counter);
             }
             assign_to_variable->inicialized = true;
             return true;
